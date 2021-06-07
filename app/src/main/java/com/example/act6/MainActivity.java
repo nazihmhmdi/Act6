3c5dcdd70499ba40518e01db1598ebfbc935c8de
Muhammad Nazih Mahmudi <nazih.mahmudi12@gmali.com>
2021-06-07 22:33:34 +0700

    Act 7

--- app/src/main/java/com/example/act6/MainActivity.java
+++ app/src/main/java/com/example/act6/MainActivity.java
@@ -4,22 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.act6.adapter.TemanAdapter;
 import com.example.act6.database.DBController;
 import com.example.act6.database.Teman;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private TemanAdapter adapter;
     private ArrayList<Teman> temanArrayList;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static String url_select = "http://10.0.2.2/umyTI/bacateman.php";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_TELPON = "telpon";
+
     DBController controller = new DBController(this);
 
     String id,nm,tlp;
@@ -47,18 +68,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void BacaData() {
-        ArrayList<HashMap<String, String>> daftarTeman = controller.getAllTeman();
-        temanArrayList = new ArrayList<>();
-        //memindah dari hasil query kedalam Teman
-        for (int i=0; i<daftarTeman.size();i++) {
-            Teman teman = new Teman();
+        Context context;
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        JsonArrayRequest jArr = new JsonArrayRequest(url_select, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.d(TAG, response.toString());
 
-            teman.setId(daftarTeman.get(i).get("id").toString());
-            teman.setNama(daftarTeman.get(i).get("nama".toString()));
-            teman.setTelpon(daftarTeman.get(i).get("telpon".toString()));
-            //pindahkan dari Teman kedalam Arraylist teman di adapter
-            temanArrayList.add(teman);
+                //Parsing json
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject obj = response.getJSONObject(i);
+                        Teman item = new Teman();
+                        item.setId(obj.getString(TAG_ID));
+                        item.setNama(obj.getString(TAG_NAMA));
+                        item.setTelpon(obj.getString(TAG_TELPON));
 
-        }
+                        //menambah item ke array
+                        temanArrayList.add(item);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                error.printStackTrace();
+                Toast.makeText(MainActivity.this, "Gagal", Toast.LENGTH_SHORT). show();
+            }
+        });
+        requestQueue.add(jArr);
     }
 }
